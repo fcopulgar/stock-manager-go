09a969c109cf95e456d161f2fcb0d38d55f215ed
fpulgar <fpulgar@pm.me>
2024-11-11 00:26:44 -0300

    add config file

--- config/config.go
+++ config/config.go
@@ -1,1 +1,19 @@
 package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadConfig() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("The .env file could not be loaded, system environment variables will be used.")
+	}
+}
+
+func GetEnv(key string) string {
+	return os.Getenv(key)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/fcopulgar/stock-manager-go
 
 go 1.23.3
+
+require github.com/joho/godotenv v1.5.1 // indirect
